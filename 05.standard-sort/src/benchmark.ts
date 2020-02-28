deaaca41c0bd5f8d2234c7354f8ed886dc551069
AeroCode <meteo_meteo@naver.com>
2020-02-28 20:10:17 +0900

    update: display progress

--- 05.standard-sort/src/benchmark.ts
+++ 05.standard-sort/src/benchmark.ts
@@ -44,6 +44,10 @@ export function benchmark<T>(targets: BenchmarkTarget<T>[], arg: AddonParamType,
         let totalExecutionTime: number = 0;
         for (let i = 0; i < repeat; i++) {
             //
+            // prints the progress.
+            process.stdout.write(`Test in progress... ${i} / ${repeat}\r`);
+
+            //
             // execute function then save into "thisTestOutput".
             let thisTestOutput: BenchmarkTargetFunctionReturn<T> | undefined;
             totalExecutionTime += getExecutionTime(() => {
@@ -51,7 +55,7 @@ export function benchmark<T>(targets: BenchmarkTarget<T>[], arg: AddonParamType,
             });
 
             //
-            // Get internal time information.
+            // get internal time information.
             for (const key in thisTestOutput?.statics) {
                 if (statics[key] === undefined) statics[key] = 0;
                 statics[key] += thisTestOutput?.statics[key];
@@ -61,14 +65,18 @@ export function benchmark<T>(targets: BenchmarkTarget<T>[], arg: AddonParamType,
         //
         // Print result.
         let etc = totalExecutionTime / repeat / 1e3;
+        let subCnt = 0;
         console.log(`[${target.name}] ${etc} us`);
         console.group();
         for (const key in statics) {
             const thisPhaseExecutionTime = statics[key] / repeat / 1e3;
             etc -= thisPhaseExecutionTime;
+            subCnt++;
             console.log(`> ${key} : ${thisPhaseExecutionTime} us`);
         }
-        console.log(`> etc : ${etc} us`);
+        if (subCnt !== 0) {
+            console.log(`> etc : ${etc} us`);
+        }
         console.groupEnd();
     }
     console.groupEnd();
